f5664a5efd5a103c9e4aa40e048ca2f3c36c5207
iampkuhz <iampkuhz@gmail.com>
2014-10-11 16:07:55 +0800

    U400:generate git test pro finished, remaining to be test

--- src/extract/predicatetable/GenerateTestCase.java
+++ src/extract/predicatetable/GenerateTestCase.java
@@ -3,6 +3,7 @@ package extract.predicatetable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 import extract.pageinfo.myPredicateAvg;
@@ -14,7 +15,7 @@ public class GenerateTestCase {
 
 	public static void start(){
 		uFunc.AlertPath = "data/info/predicate/GenerateTestCase.info";
-		String folder = "";
+		String folder = "/home/hanzhe/Public/result_hz/wiki_count2/";
 		
 		// ("id1 \t id2")
 		HashMap<String, Boolean> TagedPair = 
@@ -23,8 +24,8 @@ public class GenerateTestCase {
 		
 		HashMap<Integer, myPredicateAvg> predicatesMap =
 				new HashMap<Integer, myPredicateAvg>();
-		LoadPredAvg(predicatesMap, );
-		long startTime = System.currentTimeMillis();
+		int MaxId = LoadPredAvg(predicatesMap, folder + "PredicateAvg");
+		
 		info = "input next size of pair you want to label."
 				+ " input 0 for end. Program will save the file "
 				+ "after finished this task.\ntarget size:";
@@ -32,12 +33,89 @@ public class GenerateTestCase {
 		Scanner sc = new Scanner(System.in);
 		int nextSize = sc.nextInt();
 		int curNr = 0;
+		long startTime = System.currentTimeMillis();
+		Random rand = new Random();
 		while(curNr < nextSize)
 		{
-			
+			int id1 = rand.nextInt(MaxId + 1);
+			int id2;
+			while(true){
+				id2 = rand.nextInt(MaxId + 1);
+				if(id2 != id1)
+					break;
+			}
+			if(id1 > id2){
+				int tmp = id1;
+				id1 = id2;
+				id2 = tmp;
+			}
+			String key1 = id1 + "\t" + id2;
+			if(TagedPair.containsKey(key1))
+				continue;
+			else{
+				boolean isSim = false;
+				System.out.println(predicatesMap.get(id1).GetHintInfo() 
+						+ "\n" + predicatesMap.get(id2).GetHintInfo());
+				String feedback = sc.nextLine();
+				if(feedback.equals("1"))
+					isSim = true;
+				TagedPair.put(key1, isSim);
+				curNr ++;
+			}
 		}
-		
+		sc.close();
+		info = "total cost:" + uFunc.GetTime(System.currentTimeMillis() - startTime)
+				+ "\t" + " per cost:" + (System.currentTimeMillis() - startTime)/nextSize 
+				+ "minsec";
+		uFunc.Alert(true, c, info);
 		uFunc.AlertClose();
+	}
+
+	private static int LoadPredAvg(
+			HashMap<Integer, myPredicateAvg> predicateAvg, String path) {
+		// TODO Auto-generated method stub
+		String oneLine = "";
+		int MaxId = 0;
+		BufferedReader br = uFunc.getBufferedReader(path);
+		try {
+			while((oneLine = br.readLine()) != null)
+			{
+				if(oneLine.equals(""))
+					continue;
+				int id = 0;
+				String Content;
+				if(oneLine.startsWith("id:")){
+					id = Integer.parseInt(oneLine.substring(oneLine.indexOf(":") + 1));
+					if(id > MaxId)
+						MaxId = id;
+					if(predicateAvg.containsKey(id)){
+						System.out.println("predicateAvg content init error:" + oneLine);
+						return MaxId;
+					}
+					oneLine = br.readLine();
+					if(oneLine.startsWith("Content:")){
+						Content = oneLine.substring(oneLine.indexOf(":") + 1);
+						myPredicateAvg one = new myPredicateAvg(Content, id);
+						one.CompleteInfo(br);
+						predicateAvg.put(id, one);
+					}
+					else{
+						System.out.println("predicateAvg content init error:" + oneLine);
+						return MaxId ;
+					}
+				}
+				else{
+					System.out.println("predicateAvg id init error:" + oneLine);
+					return MaxId;
+				}
+				
+			}
+			info = "predicateAvg map size:" + predicateAvg.size();
+			uFunc.Alert(true, c, info);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return MaxId;
 	}
 
 	private static void LoadTagedpair(HashMap<String, Boolean> tagedPair,
